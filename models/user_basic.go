fd2bc1bfbe68a07eaf1833c05605432986dfc684
tanqingyang999 <1131534180@qq.com>
2025-08-28 18:43:45 +0800

    列表

--- models/user_basic.go
+++ models/user_basic.go
@@ -11,21 +11,19 @@ var UserBasicModel = UserBasic{}
 
 type UserBasic struct {
 	gorm.Model
-	Id            uint64            `gorm:"column:id;size:64"             json:"id"`
+	ID            uint64            `gorm:"column:id;size:64"             json:"id"`
 	Username      string            `gorm:"column:username;size:32"       json:"username"`
 	Password      string            `gorm:"column:password;size:64"       json:"password"`
 	Phone         string            `gorm:"column:phone;size:64"          json:"phone"`
 	Email         string            `gorm:"column:email;size:64"          json:"email"`
 	Identity      string            `gorm:"column:identity;size:64"       json:"identity"`
-	ClientIp      string            `gorm:"column:client_ip;size:64"      json:"clientIp"`
+	ClientIP      string            `gorm:"column:client_ip;size:64"      json:"clientIp"`
 	ClientPort    string            `gorm:"column:client_port;size:64"    json:"clientPort"`
 	LoginTime     common.CustomTime `gorm:"column:login_time"             json:"loginTime"`
 	HeartbeatTime common.CustomTime `gorm:"column:heartbeat_time"         json:"heartbeatTime"`
 	LogoutTime    common.CustomTime `gorm:"column:logout_time"            json:"logoutTime"`
 	IsLogout      int               `gorm:"column:is_logout;size:64"      json:"isLogout"`
 	DeviceInfo    string            `gorm:"column:device_info;size:64"    json:"deviceInfo"`
-	CreatedAt     time.Time         `gorm:"column:created_at"             json:"createdAt"`
-	UpdatedAt     time.Time         `gorm:"column:updated_at"             json:"updatedAt"`
 }
 
 // 设置表名称  默认的表明会带s  链接表名会变成users
@@ -68,7 +66,7 @@ func (ub *UserBasic) CreateUser(req *model.CreateUserRequest) (err error) {
 		Phone:         req.Phone,
 		Email:         req.Email,
 		Identity:      req.Identity,
-		ClientIp:      req.ClientIp,
+		ClientIP:      req.ClientIp,
 		ClientPort:    req.ClientPort,
 		LoginTime:     loginTime,
 		HeartbeatTime: heartbeatTime,
@@ -138,7 +136,7 @@ func (ub *UserBasic) UpdateUser(req *model.UpdateUserRequest) (err error) {
 		"Phone":      req.Phone,
 		"Email":      req.Email,
 		"Identity":   req.Identity,
-		"ClientIp":   req.ClientIp,
+		"ClientIP":   req.ClientIp,
 		"ClientPort": req.ClientPort,
 		"IsLogout":   req.IsLogout,
 		"DeviceInfo": req.DeviceInfo,
